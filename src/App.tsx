f5d4435b8193b0abaf06af87f1672c31285debf5
Vitaliy Lipin <vit.lipin@gmail.com>
2021-05-14 14:53:03 +0300

    added styles to categories

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,36 @@ import { Categories } from './Components/Categories/Categories'
 import { ItemSofa } from './Components/Categories/ItemTypes/Sofas'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { ItemChairs } from './Components/Categories/ItemTypes/Chairs';
+import { ItemBed } from './Components/Categories/ItemTypes/Beds';
+import { ItemDesk } from './Components/Categories/ItemTypes/Desks';
+import { ItemCupboard } from './Components/Categories/ItemTypes/Cupboards';
+import { ItemCarpet } from './Components/Categories/ItemTypes/Carpets';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <Router>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/categories">Categories</Link></li>
-              <li><Link to="/gallery">Gallery</Link></li>
-              <li><Link to="/articles">Articles</Link></li>
-            </ul>
-          </nav>
+    <div className='App'>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/categories'>Categories</Link></li>
+            <li><Link to='/gallery'>Gallery</Link></li>
+            <li><Link to='/articles'>Articles</Link></li>
+          </ul>
+        </nav>
 
-          <Switch>
-            <Route path="/categories">
-              <Categories />
-            </Route>
-            <Route exact path="/sofas"><ItemSofa /></Route>
-            <Route exact path="/chairs"><ItemChairs /></Route>
-          </Switch>
-        </Router>
-      </div>
+        <Switch>
+          <Route path='/categories'>
+            <Categories />
+          </Route>
+          <Route exact path='/beds'><ItemBed /></Route>
+          <Route exact path='/chairs'><ItemChairs /></Route>
+          <Route exact path='/desks'><ItemDesk /></Route>
+          <Route exact path='/cupboards'><ItemCupboard /></Route>
+          <Route exact path='/sofas'><ItemSofa /></Route>
+          <Route exact path='/carpets'><ItemCarpet /></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/Categories/Categories.tsx
+++ src/Components/Categories/Categories.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './categories.scss'
 
 export const Categories = () => {
 
     return (
-        <nav>
-            <ul>
-                <li><Link to="/sofas">Sofas</Link></li>
-                <li><Link to="/chairs">Chairs</Link></li>
+        <nav className='categories__Navigation'>
+            <ul className='categories__List'>
+                <li className='categories__Section categories__Section-beds'>
+                    <span className='categories__Section-name'>Beds</span>
+                    <Link className='categories__Section-link' to='/beds'>shop now</Link>
+                </li>
+                <li className='categories__Section categories__Section-chairs'>
+                    <span className='categories__Section-name'>Chairs</span>
+                    <Link className='categories__Section-link' to='/chairs'>shop now</Link>
+                </li>
+                <li className='categories__Section categories__Section-desks'>
+                    <span className='categories__Section-name'>Desks</span>
+                    <Link className='categories__Section-link' to='/desks'>shop now</Link>
+                </li>
+                <li className='categories__Section categories__Section-cupboards'>
+                    <span className='categories__Section-name'>Cupboards</span>
+                    <Link className='categories__Section-link' to='/cupboards'>shop now</Link>
+                </li>
+                <li className='categories__Section categories__Section-sofas'>
+                    <span className='categories__Section-name'>Sofas</span>
+                    <Link className='categories__Section-link' to='/sofas'>shop now</Link>
+                </li>
+                <li className='categories__Section categories__Section-carpets'>
+                    <span className='categories__Section-name'>Carpets</span>
+                    <Link className='categories__Section-link' to='/carpets'>shop now</Link>
+                </li>
             </ul>
         </nav>
     )
