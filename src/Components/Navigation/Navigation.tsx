b098bc5539de3f7ffacc47a8cce02e6aa67b47b1
LipinVV <vit.lipin@gmail.com>
2021-06-13 11:58:24 +0300

    added finished logic and ui of mobile menu navigation

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {Link, Route} from "react-router-dom";
 import React, {useState} from "react";
 import './navigation.scss'
 
@@ -7,6 +7,10 @@ export const Navigation = () => {
     const [menu, setMenu] = useState(false);
     const menuToggleHandler = () => {
         setMenu(!menu)
+    }
+    const [subMenu, setSubMenu] = useState(true);
+    const subMenuToggleHandler = () => {
+        setSubMenu(!subMenu)
     }
     return (
         <nav className='app__navigation'>
@@ -20,14 +24,26 @@ export const Navigation = () => {
                     <button onClick={menuToggleHandler} className='navigation__menu-btn'></button>
                 </li>
             </ul>
-
             {menu ? <ul className='app__links'>
-                <li className='app__link'><Link className='app__link-route' to='/categories'>Category</Link></li>
+                <li onClick={subMenuToggleHandler} className='app__link'>Categories
+                        {!subMenu ?
+                            <div className='app__link-subroutes'>
+                                <Link className='app__link-subroute' to='/categories'>All Categories</Link>
+                                <Link className='app__link-subroute' to='/sofas'>Sofas</Link>
+                                <Link className='app__link-subroute' to='/chairs'>Chairs</Link>
+                                <Link className='app__link-subroute' to='/desks'>Desks</Link>
+                                <Link className='app__link-subroute' to='/beds'>Beds</Link>
+                                <Link className='app__link-subroute' to='/carpets'>Carpets</Link>
+                                <Link className='app__link-subroute' to='/cupboards'>Cupboards</Link>
+                            </div>
+                            : null}
+                </li>
                 <li className='app__link'><Link className='app__link-route' to='/shoppingCart'>Shopping Cart</Link></li>
                 <li className='app__link'><Link className='app__link-route' to='/wishlist'>WishList</Link></li>
                 <li className='app__link'><Link className='app__link-route' to='/'>Sign Up</Link></li>
                 <li className='app__link'><Link className='app__link-route' to='/'>Login</Link></li>
             </ul> : null}
         </nav>
+
     )
 }
